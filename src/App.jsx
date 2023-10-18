b4aae1f318ad37dd26844f2ddf44baf4488fffa2
Athul Vijayan <web.wizard.athul@gmail.com>
2023-10-18 23:08:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+
+function App(){
+  return(
+    <h1 className="text-white">athul</h1>
+  )
+}
+
+export default App;
